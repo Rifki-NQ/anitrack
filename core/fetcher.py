2a7027f1fbab3bb1133f2ed54498e735a5829196
RIFKI-NQ <rifqinf950@gmail.com>
2026-03-26 20:15:14 +0700

    feat(fetcher): add Anilist fetcher

--- core/fetcher.py
+++ core/fetcher.py
@@ -1,5 +1,6 @@
+import requests
 from abc import ABC, abstractmethod
-from typing import Literal, get_args
+from typing import Literal, get_args, Any
 from core.exceptions import InvalidDataSource
 
 class FetchData(ABC):
@@ -14,11 +15,30 @@ class FetchData(ABC):
             raise InvalidDataSource(f"Invalid data source provided ({data_source}), expected ({FetchData.VALID_DATA_SOURCES})")
         
     @abstractmethod
-    def fetch_data(self, query: str):
+    def fetch_data(self, anime_title: str) -> dict[str, Any]:
         pass
+    
+    def _request(self, url: str, query: str, variables: dict[str, str]) -> requests.Response:
+        response = requests.post(url, json={"query": query, "variables": variables})
+        return response
     
 class FetchAnilist(FetchData):
     BASE_URL = "https://graphql.anilist.co"
+    QUERY = """
+    query ($search: String) {
+        Media (search: $search, type: ANIME) {
+            id
+            title {
+                english
+                romaji
+            }
+            averageScore
+            episodes
+            genres
+        }
+    }
+    """
     
-    def fetch_data(self, query: str):
-        pass
+    def fetch_data(self, anime_title: str) -> dict[str, Any]:
+        data = self._request(url=self.BASE_URL, query=self.QUERY, variables={"search": anime_title})
+        return data.json()

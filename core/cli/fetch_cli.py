79929853ceb6576dd84f47271ea00bbb760a9b8a
RIFKI-NQ <rifqinf950@gmail.com>
2026-04-01 15:26:03 +0700

    refactor(fetch_cli): catch raised fetcher exceptions in cli

--- core/cli/fetch_cli.py
+++ core/cli/fetch_cli.py
@@ -1,6 +1,7 @@
 import argparse
 from dataclasses import asdict
 from core.normalizer import ResponseNormalizer
+from core.exceptions import FetcherError
 
 class FetchCLI:
     def __init__(self, normalizer: ResponseNormalizer) -> None:
@@ -8,11 +9,21 @@ class FetchCLI:
     
     def handle_fetch(self, args: argparse.Namespace) -> None:
         if args.title: #search by title
-            data = asdict(self.normalizer.get_anime_data_by_title(source=args.source, anime_title=args.title))
+            try:
+                data = asdict(self.normalizer.get_anime_data_by_title(source=args.source, anime_title=args.title))
+            except FetcherError as e:
+                print(e)
+                return
+            
             for key, value in data.items():
                 print(f"{key}: {value}")
                 
         elif args.id: #search by id
-            data = asdict(self.normalizer.get_anime_data_by_id(source=args.source, anime_id=args.id))
+            try:
+                data = asdict(self.normalizer.get_anime_data_by_id(source=args.source, anime_id=args.id))
+            except FetcherError as e:
+                print(e)
+                return
+            
             for key, value in data.items():
                 print(f"{key}: {value}")

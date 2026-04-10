ce4f6f1b2fd7541bd016edda5acf87f12ed69f61
RIFKI-NQ <rifqinf950@gmail.com>
2026-04-10 21:28:26 +0700

    refactor(normalizer): add helper method _to_anime_model to convert fetched data into AnimeDataModel

--- core/normalizer.py
+++ core/normalizer.py
@@ -1,6 +1,7 @@
 from core.models.anime_model import AnimeDataModel, DATA_SOURCES, VALID_DATA_SOURCES
 from core.models.protocols import FetchersProtocol
 from core.exceptions import InvalidDataSource
+from typing import Any
 
 class ResponseNormalizer:
     def __init__(self, anilist_fetcher: FetchersProtocol, jikan_fetcher: FetchersProtocol) -> None:
@@ -8,69 +9,68 @@ class ResponseNormalizer:
         self.jikan_fetcher = jikan_fetcher
         
     def get_anime_data_by_title(self, source: DATA_SOURCES, anime_title: str, entry_number: int) -> AnimeDataModel:
+        self._validate_data_source(source)
         if source == "anilist":
             return self._get_anilist_data_by_title(anime_title, entry_number)
         elif source == "jikan":
             return self._get_jikan_data_by_title(anime_title, entry_number)
-        else:
-            raise InvalidDataSource(f"Invalid data source provided ({source}), expected ({VALID_DATA_SOURCES})")
     
     def get_all_anime_data_by_title(self, source: DATA_SOURCES, anime_title: str, max_entry: int | None = None) -> list[AnimeDataModel]:
-        data_mapping = {
+        self._validate_data_source(source)
+        data_source = {
             "anilist": self.anilist_fetcher.fetch_data_by_title,
             "jikan": self.jikan_fetcher.fetch_data_by_title
         }
-        data_collection = data_mapping[source](anime_title)
+        data_converter = {
+            "anilist": self._anilist_to_anime_model,
+            "jikan": self._jikan_to_anime_model
+        }
+        data_collection = data_source[source](anime_title)
         data_model_list: list[AnimeDataModel] = []
         for entry_num, data in enumerate(data_collection, 1):
             #inclusive for max_entry value
             if max_entry is not None and entry_num > max_entry: 
                 break
-            if source == "anilist":
-                data_model_list.append(AnimeDataModel(source="anilist", id=data["id"],
-                                                      english_title=data["title"]["english"], romaji_title=data["title"]["romaji"],
-                                                      average_score=data["averageScore"], episodes=data["episodes"], genres=data["genres"]))
-            elif source == "jikan":
-                data_model_list.append(AnimeDataModel(source="jikan", id=data["mal_id"],
-                                                      english_title=data["title_english"], romaji_title=data["title"],
-                                                      average_score=data["score"], episodes=data["episodes"], genres=[g["name"] for g in data["genres"]]))
+            data_model_list.append(data_converter[source](data))
         return data_model_list
     
     def get_anime_data_by_id(self, source: DATA_SOURCES, anime_id: int) -> AnimeDataModel:
+        self._validate_data_source(source)
         if source == "anilist":
             return self._get_anilist_data_by_id(anime_id)
         elif source == "jikan":
             return self._get_jikan_data_by_id(anime_id)
-        else:
-            raise InvalidDataSource(f"Invalid data source provided ({source}), expected ({VALID_DATA_SOURCES})")
     
     def _get_anilist_data_by_title(self, anime_title: str, entry_number: int) -> AnimeDataModel:
         data = self.anilist_fetcher.fetch_data_by_title(anime_title)[entry_number]
+        return self._anilist_to_anime_model(data)
+        
+    def _get_anilist_data_by_id(self, anime_id: int) -> AnimeDataModel:
+        data = self.anilist_fetcher.fetch_data_by_id(anime_id)
+        return self._anilist_to_anime_model(data)
+
+    def _get_jikan_data_by_title(self, anime_title: str, entry_number: int) -> AnimeDataModel:
+        data = self.jikan_fetcher.fetch_data_by_title(anime_title)[entry_number]
+        return self._jikan_to_anime_model(data)
+        
+    def _get_jikan_data_by_id(self, anime_id: int) -> AnimeDataModel:
+        data = self.jikan_fetcher.fetch_data_by_id(anime_id)
+        return self._jikan_to_anime_model(data)
+    
+    def _anilist_to_anime_model(self, data: dict[str, Any]) -> AnimeDataModel:
         return AnimeDataModel(
             source="anilist", id=data["id"],
             english_title=data["title"]["english"], romaji_title=data["title"]["romaji"],
             average_score=data["averageScore"], episodes=data["episodes"], genres=data["genres"]
         )
-        
-    def _get_anilist_data_by_id(self, anime_id: int) -> AnimeDataModel:
-        data = self.anilist_fetcher.fetch_data_by_id(anime_id)
-        return AnimeDataModel(
-            source="anilist", id=data["id"],
-            english_title=data["title"]["english"], romaji_title=data["title"]["romaji"],
-            average_score=data["averageScore"], episodes=data["episodes"], genres=data["genres"])
-
-    def _get_jikan_data_by_title(self, anime_title: str, entry_number: int) -> AnimeDataModel:
-        data = self.jikan_fetcher.fetch_data_by_title(anime_title)[entry_number]
+    
+    def _jikan_to_anime_model(self, data: dict[str, Any]) -> AnimeDataModel:
         return AnimeDataModel(
             source="jikan", id=data["mal_id"],
             english_title=data["title_english"], romaji_title=data["title"],
             average_score=data["score"], episodes=data["episodes"], genres=[g["name"] for g in data["genres"]]
         )
         
-    def _get_jikan_data_by_id(self, anime_id: int) -> AnimeDataModel:
-        data = self.jikan_fetcher.fetch_data_by_id(anime_id)
-        return AnimeDataModel(
-            source="jikan", id=data["mal_id"],
-            english_title=data["title_english"], romaji_title=data["title"],
-            average_score=data["score"], episodes=data["episodes"], genres=[g["name"] for g in data["genres"]]
-        )
+    def _validate_data_source(self, source: DATA_SOURCES) -> None:
+        if source not in VALID_DATA_SOURCES:
+            raise InvalidDataSource(f"Invalid data source provided ({source}), expected ({VALID_DATA_SOURCES})")

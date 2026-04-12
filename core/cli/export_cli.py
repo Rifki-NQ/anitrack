7535ec2718b761c2ecedaf30f8c68bd6a85f90eb
RIFKI-NQ <rifqinf950@gmail.com>
2026-04-12 10:20:48 +0700

    refactor(file_handler): make file_handler methods receive AnimeDataModel instead of dict

--- core/cli/export_cli.py
+++ core/cli/export_cli.py
@@ -1,7 +1,4 @@
 from argparse import Namespace
-from dataclasses import asdict
-from pathlib import Path
-from core.models.anime_model import AnimeDataModel
 from core.normalizer import ResponseNormalizer
 from core.file_handler import DataIO
 from core.utils import get_all_data_by_title, get_all_data_by_id
@@ -24,11 +21,11 @@ class ExportCLI:
             if args.source == "all":
                 self._handle_source_all_by_title(args)
             elif args.save_all:
-                data_list = self.normalizer.get_all_anime_data_by_title(source=args.source, anime_title=args.title)
-                self._save_data_list(data_list, path=args.path, overwrite=args.overwrite)
+                data_list = self.normalizer.get_all_anime_data_by_title(args.source, args.title)
+                self.file_handler.save_all_data(data_list, args.path, args.overwrite)
             else:
-                data = asdict(self.normalizer.get_anime_data_by_title(source=args.source, anime_title=args.title, entry_number=args.entry))
-                self.file_handler.add_new_data(new_data=data, filepath=args.path, overwrite=args.overwrite)
+                data = self.normalizer.get_anime_data_by_title(args.source, args.title, args.entry)
+                self.file_handler.save_data(data, args.path, args.overwrite)
         except FetcherError as e:
             print(e)
             return
@@ -38,25 +35,24 @@ class ExportCLI:
             if args.source == "all":
                 self._handle_source_all_by_id(args)
             else:
-                data = asdict(self.normalizer.get_anime_data_by_id(source=args.source, anime_id=args.id))
-                self.file_handler.add_new_data(new_data=data, filepath=args.path, overwrite=args.overwrite)
+                data = self.normalizer.get_anime_data_by_id(args.source, args.id)
+                self.file_handler.save_data(data, args.path, args.overwrite)
         except FetcherError as e:
             print(e)
             return
         
     def _handle_source_all_by_title(self, args: Namespace) -> None:
         data_collection = get_all_data_by_title(args, self.normalizer)
-        overwrite = args.overwrite
         for data_list in data_collection:
-            self._save_data_list(data_list, args.path, overwrite)
-            overwrite = False
+            #handle --entry
+            if args.entry:
+                entry: int = args.entry
+                self.file_handler.save_data(data_list[entry], args.path, args.overwrite)
+            #handle --save-all
+            else:
+                self.file_handler.save_all_data(data_list, args.path, args.overwrite)
+                args.overwrite = False
 
     def _handle_source_all_by_id(self, args: Namespace) -> None:
-        data1, data2 = get_all_data_by_id(args, self.normalizer)
-        data_list = [data1, data2]
-        self._save_data_list(data_list, args.path, args.overwrite)
-
-    def _save_data_list(self, data_list: list[AnimeDataModel], path: Path, overwrite: bool) -> None:
-        for data in data_list:
-            self.file_handler.add_new_data(asdict(data), path, overwrite)
-            overwrite = False
+        data_list = get_all_data_by_id(args, self.normalizer)
+        self.file_handler.save_all_data(data_list, args.path, args.overwrite)
--- core/file_handler.py
+++ core/file_handler.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
-from typing import Any
-from dataclasses import fields
+from typing import Sequence
+from dataclasses import fields, asdict
 from argparse import ArgumentTypeError
 from core.models.anime_model import AnimeDataModel
 
@@ -16,9 +16,22 @@ def valid_filepath(filepath: str) -> Path:
     return Path(filepath)
 
 class DataIO:
-    def add_new_data(self, new_data: dict[str, Any], filepath: Path, overwrite: bool = False) -> None:
-        self._save_file(new_data, filepath, overwrite)
-    
+    def save_data(self, new_data: AnimeDataModel, filepath: Path, overwrite: bool = False) -> None:
+        df_new_data = pd.DataFrame([asdict(new_data)])
+        
+        if overwrite:
+            df_new_data.to_csv(filepath, index=False)
+            return
+        
+        df_previous_data = self._read_file(filepath)
+        df_merged_data = pd.concat([df_previous_data, df_new_data], ignore_index=True)
+        df_merged_data.to_csv(filepath, index=False)
+        
+    def save_all_data(self, new_data_list: Sequence[AnimeDataModel], filepath: Path, overwrite: bool = False) -> None:
+        for data in new_data_list:
+            self.save_data(data, filepath, overwrite)
+            overwrite = False
+        
     def _read_file(self, filepath: Path) -> pd.DataFrame:
         try:
             return pd.read_csv(filepath)
@@ -28,17 +41,6 @@ class DataIO:
             #cli guarantee the file is in storage/
             self._get_empty_dataframe_model(AnimeDataModel).to_csv(filepath, index=False)
             return pd.read_csv(filepath)
-    
-    def _save_file(self, new_data: dict[str, Any], filepath: Path, overwrite: bool = False) -> None:
-        df_new_data = pd.DataFrame([new_data])
-        
-        if overwrite:
-            df_new_data.to_csv(filepath, index=False)
-            return
-        
-        df_previous_data = self._read_file(filepath)
-        df_merged_data = pd.concat([df_previous_data, df_new_data], ignore_index=True)
-        df_merged_data.to_csv(filepath, index=False)
         
     def _get_empty_dataframe_model(self, model: type[AnimeDataModel]) -> pd.DataFrame:
         return pd.DataFrame(columns=[f.name for f in fields(model)])

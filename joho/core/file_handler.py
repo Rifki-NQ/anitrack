269e97f95acffd2043455b6338cd0d9383cfb399
Rifki-NQ <rifqinf950@gmail.com>
2026-05-02 21:28:15 +0700

    fix(file_handler): fix crash when folder storage/ does not exist

--- joho/core/file_handler.py
+++ joho/core/file_handler.py
@@ -10,12 +10,13 @@ class DataIO:
 
     def save_data(self, data: AnimeDataModel, overwrite: bool) -> None:
         overwrite_map = {True: "w", False: "a"}
+        self.filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        file_exist = self.filepath.exists()
+        is_empty_file = self.filepath.stat().st_size == 0 if file_exist else True
         with open(self.filepath, mode=overwrite_map[overwrite], newline="") as f:
             fieldnames = [adm.name for adm in fields(AnimeDataModel)]
             writer = csv.DictWriter(f, fieldnames=fieldnames)
-            if self._is_empty_file():
+            if overwrite or is_empty_file:
                 writer.writeheader()
             writer.writerow(asdict(data))
-
-    def _is_empty_file(self) -> bool:
-        return self.filepath.stat().st_size == 0

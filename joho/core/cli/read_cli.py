8ec358ec392a3e30c1bc887f1d8353421bfeae5e
Rifki-NQ <rifqinf950@gmail.com>
2026-05-03 19:28:43 +0700

    refactor(read_cli): catch FileHandler errors in cli

--- joho/core/cli/read_cli.py
+++ joho/core/cli/read_cli.py
@@ -1,4 +1,5 @@
 from joho.core.file_handler import DataIO
+from joho.core.exceptions import FileHandlerError
 
 
 class ReadCLI:
@@ -6,13 +7,18 @@ class ReadCLI:
         self.file_handler = file_handler
 
     def handle_read_cli(self, entry: int | None) -> None:
+        try:
+            all_data = self.file_handler.read_data()
+        except FileHandlerError as e:
+            print(e)
+            return
         if entry is not None:
             try:
-                self._show_entry(self.file_handler.read_data()[entry])
+                self._show_entry(all_data[entry])
             except IndexError:
                 print(f"Error: out of bound entry index: {entry}, for file: {self.file_handler.filepath}")
             return
-        self._show_entries(self.file_handler.read_data())
+        self._show_entries(all_data)
 
     def _show_entry(self, entry_data: dict[str, str]) -> None:
         for key, value in entry_data.items():
